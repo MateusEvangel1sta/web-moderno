5bfac2fa516d44980bf3573369d7c98ddbc93612
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-09 22:36:04 -0300

    Seção SQL - Save

--- database/bancoRelacional/InserirEstados1.sql
+++ database/bancoRelacional/InserirEstados1.sql
@@ -1,20 +1,9 @@
-INSERT INTO estados (
-  nome,
-  sigla,
-  regiao,
-  populacao
-);
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES
+  ('Acre', 'AC', 'Norte', 0.83);
 
-VALUES('Acre', 'AC', 'Norte', 0.83);
-
-INSERT INTO estados (
-  nome,
-  sigla,
-  regiao,
-  populacao
-);
-
-VALUE
-  ('Alagoas', 'AL', 'Nordeste', 3.38);
-  ('Amapá', 'AP', 'Norte', 0.8);
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES
+  ('Alagoas', 'AL', 'Nordeste', 3.38),
+  ('Amapá', 'AP', 'Norte', 0.8),
   ('Amazonas', 'AM', 'Norte', 4.06);
