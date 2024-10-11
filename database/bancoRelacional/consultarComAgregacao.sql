3636207776de6ff4a32392e6613df96b34c038d5
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-11 18:52:11 -0300

    Seção SQL - Save

---
+++ database/bancoRelacional/consultarComAgregacao.sql
@@ -0,0 +1,11 @@
+SELECT
+  regiao AS 'Região',
+  sum(populacao) AS Total
+FROM estados
+GROUP BY regiao
+ORDER BY Total DESC
+
+
+SELECT
+  sum(populacao) AS Total
+FROM estados
---
+++ database/bancoRelacional/inserirNovosEstados.sql
@@ -0,0 +1,23 @@
+INSERT INTO estados (
+  id,
+  nome,
+  sigla,
+  regiao,
+  populacao
+)
+
+VALUES (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+SELECT * FROM estados
+
+
+INSERT INTO estados (
+  nome,
+  sigla,
+  regiao,
+  populacao
+)
+
+VALUES ('Mais Novo', 'MN', 'NORTE', 2.51)
+
+SELECT * FROM estados
