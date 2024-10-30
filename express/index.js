9b6ba5ee3399d28df78ae0b6af27be82ab074628
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-10-30 20:17:21 -0300

    Seção Express - Save

--- express/index.js
+++ express/index.js
@@ -3,8 +3,20 @@ const app = express();
 
 const port = 3000;
 
-app.use((req, res) => {
-  res.send('Estou bem!');
+app.get('/opa', (req, res) => {
+  res.json([
+    { id: 7, name: 'Ana', position: 1 },
+    { id: 24, name: 'Bia', position: 2 },
+    { id: 73, name: 'Carlos', position: 3 },
+  ]);
+  
+  // res.json({
+  //   name: 'Ipad 32GB',
+  //   price: 1899.00,
+  //   discount: 0.12
+  // });
+
+  // res.send('Estou bem!');
 });
 
 app.listen(port, () => {
