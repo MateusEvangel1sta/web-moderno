1ef65a57bbd8d6447d8bc9a006de8e003b97cd30
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2024-11-10 19:42:30 -0300

    Seção Express - Save

--- express/index.js
+++ express/index.js
@@ -3,12 +3,25 @@ const app = express();
 
 const port = 3000;
 
-app.get('/opa', (req, res) => {
-  res.json([
-    { id: 7, name: 'Ana', position: 1 },
-    { id: 24, name: 'Bia', position: 2 },
-    { id: 73, name: 'Carlos', position: 3 },
-  ]);
+app.use('/opa', (req, res, next) => {
+  console.log('Antes...');
+  next();
+});
+
+app.get('/opa', (req, res, next) => {
+  console.log('Durante...')
+  res.json({
+      data: [{ id: 7, name: 'Ana', position: 1 },
+      { id: 24, name: 'Bia', position: 2 },
+      { id: 73, name: 'Carlos', position: 3 },
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  });
+  
+  next();
   
   // res.json({
   //   name: 'Ipad 32GB',
@@ -17,6 +30,10 @@ app.get('/opa', (req, res) => {
   // });
 
   // res.send('Estou bem!');
+});
+
+app.use('/opa', (req, res) => {
+  console.log('Depois...');
 });
 
 app.listen(port, () => {
